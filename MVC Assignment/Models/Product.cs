bd82d5136d61301f6a90d65730f7ab81cf5a2b53
Manohar Singh <mchauhan@covalience.io>
2026-01-09 17:03:43 +0530

    Scaffolding

--- MVC Assignment/Models/Product.cs
+++ MVC Assignment/Models/Product.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MVC_Assignment.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required")]
+        [StringLength(50, MinimumLength = 3)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 100000)]
         public decimal Price { get; set; }
         public string Description { get; set; } = string.Empty;
     }
--- MVC Assignment/Program.cs
+++ MVC Assignment/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MVC_Assignment.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("MvcDb"));
+
 
 var app = builder.Build();
 
