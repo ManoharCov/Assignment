38c085b6e3fa4d109703b5785c444068f4b538fc
Manohar Singh <mchauhan@covalience.io>
2026-01-09 17:03:43 +0530

    Scaffolding

---
+++ MVC Assignment/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Assignment.Models;
+
+namespace MVC_Assignment.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+
---
+++ MVC Assignment/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Assignment.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
