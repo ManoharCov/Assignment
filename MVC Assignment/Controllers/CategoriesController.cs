9381ec31e77b6dd120dcb2d3eeb6a656dd7f9122
Manohar Singh <mchauhan@covalience.io>
2026-01-22 11:07:52 +0530

    remove

--- MVC Assignment/Controllers/CategoriesController.cs
+++ MVC Assignment/Controllers/CategoriesController.cs
@@ -50,8 +50,6 @@ namespace MVC_Assignment.Controllers
         }
 
         // POST: Categories/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name")] Category category)
@@ -82,8 +80,6 @@ namespace MVC_Assignment.Controllers
         }
 
         // POST: Categories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category)
